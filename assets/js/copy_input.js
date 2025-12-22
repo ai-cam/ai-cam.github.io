4f6e97d944bbc3ab9988d11c29b1b5079bab6ff6
Berkay Nizam <brkynzm@gmail.com>
2025-12-22 17:46:25 +0300

    Replace implementation with AI@Cam 2026 website

--- assets/js/copy_input.js
+++
@@ -1,9 +0,0 @@
-function CopyToClipboard(containerid) {
-    var $temp = $("<textarea>");
-    $("body").append($temp);
-    
-    var str = document.getElementById(containerid).innerHTML
-    $temp.val(str).select();
-    document.execCommand("Copy");
-    $temp.remove();
-}
